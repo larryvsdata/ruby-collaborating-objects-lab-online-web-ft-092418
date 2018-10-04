1ab61f27570d37ed31b25a90f7128ed8772fdd2f
larryvsdata <ermanbekaroglu@gmail.com>
2018-10-04 23:53:44 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,8 +11,9 @@ end
   def self.new_by_filename (filename )
     song_name = filename.split("-")[1].strip
     artist_name = filename.split("-")[0].strip
-    artist= Artist.find_or_create_by_name(artist_name)
     song = Song.new(song_name)
+    song.artist= Artist.find_or_create_by_name(artist_name)
+    
     artist.add_song (song)
     song
   end
