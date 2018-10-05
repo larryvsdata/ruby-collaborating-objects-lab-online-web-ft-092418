58602762b25213865fa4f48bac4a00155f0d5b33
larryvsdata <ermanbekaroglu@gmail.com>
2018-10-05 01:13:36 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -2,13 +2,13 @@
 
 
 class MP3Importer
-  attr_accessor :path
+  attr_accessor :path , :files
   
   def initialize (directory)
     self.path = directory
   end
   
-  def import 
+  def files 
     Dir.entries(@path).select {|f| !File.directory? f}
   end
   
